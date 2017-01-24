46c78a08b435bd28701157b0cedf8477e04e51c4
Mevershed <matthew@evershed.ca>
2017-01-24 16:33:20 -0500

    created form route

--- crm.rb
+++ crm.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
+require_relative 'contact'
+require 'sinatra'
+
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Matt's CRM"
   erb :index
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get 'contacts/new' do
+  erb :new_contacts
 end
